0f6ce7cc63a8611cfa5f082ddc798ba38dfdba06
jp-valdenegro <jua.valdenegro@duocuc.cl>
2024-06-19 09:22:56 -0400

    Add files via upload

---
+++ proyecto6decimas.py
@@ -0,0 +1,10 @@
+def ingreso_empleados():
+    datos_empleados=[]
+    datos_lista=["Trabajador","Cargo","Sueldo Bruto","Desc. Salud","Desc. AFP","Liquido Pagar"]
+    nombre=input("Ingrese el nombre del empleado:\n")
+    datos_empleados.append(nombre)
+    
+    
+aaaaaaaa=ingreso_empleados()
+print(aaaaaaaa)
+    
